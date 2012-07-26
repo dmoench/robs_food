6eb36877deae64bb8a179953c5d2ae82e7815265
David Moench <david.moench@codeandtheory.com>
2012-07-26 11:36:37 -0400

    Organizing for lots of potential JS

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	// Grab json for easy manipulation
+	var json_string = $('#json_data').text();
+	var json = JSON.parse(json_string);	
+	console.log(json);
+	
+	// Google Maps
+});
