494b19277995526e7826de2ceb5ab30ac4f50620
David Moench <david.moench@codeandtheory.com>
2012-07-27 11:28:14 -0400

    Updated addressToMarker to accept a whole place object instead of just the location so the name can be accessed as well. Also tried to implement marker bouncing animation - but its not working yet.

--- js/script.js
+++ js/script.js
@@ -18,42 +18,60 @@ $(document).ready(function(){
 	
 	// Initialize the map
 	var mapOptions = {
-	  center: new google.maps.LatLng(40.715127,-74.005837),
-	  zoom: 13,
+	  center: new google.maps.LatLng(40.714997,-73.897133),
+	  zoom: 12,
 	  mapTypeId: google.maps.MapTypeId.ROADMAP
 	};
 	var map = new google.maps.Map(document.getElementById("map_canvas"),
 	    mapOptions);
 	
-	addressToMarker(json.place[0].address, map, addMarker);
+	// Place all markers on map
+	for(var i = 0; i < json.place.length; i++) {
+		addressToMarker(json.place[i], map, addMarker);
+	}
 
 });
 
 /*
 	Accepts a string address and places it as a marker on the map
-	@param address: String address
-	@param map: Reference to a map object
-	@param callback
+	@param place: A place object from the places json
+	@param map: Reference to map object
+	@param callback - call the addMarker function
 */
-function addressToMarker(address, map, callback) {
+function addressToMarker(place, map, callback) {
 	var geocoder;
 	var latlng;
 	geocoder = new google.maps.Geocoder();
-	geocoder.geocode({'address': address}, function(results, status) {
+	geocoder.geocode({'address': place.address }, function(results, status) {
 		if (status == google.maps.GeocoderStatus.OK) {
 			latlng = results[0].geometry.location;
-			callback(latlng, map);
+			callback(latlng, map, place.name);
 		} else {
 			alert("Geocode was not successful for the following reason: " + status);
 		}
 	});
 }
 
-/* Places marker on the map */
-function addMarker(latlng, map) {
+/* 
+	Places marker on the map
+	@param latlng: LatLng position object
+	@param map: Reference to map object
+	@param name: String
+*/
+function addMarker(latlng, map, name) {
 	var marker = new google.maps.Marker({
 		position: latlng,
 		map: map,
-		title: 'BIG WONG'
+		title: name
 	});
+	google.maps.event.addListener(marker, 'click', toggleBounce);
+}
+
+function toggleBounce() {
+
+  if (marker.getAnimation() != null) {
+    marker.setAnimation(null);
+  } else {
+    marker.setAnimation(google.maps.Animation.BOUNCE);
+  }
 }
